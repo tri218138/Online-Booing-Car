01d632b43fb5c3dd4beb0140c2886b154a652810
Hoang39 <87194190+Hoang39@users.noreply.github.com>
2022-12-13 10:00:20 +0700

    Update dbms.py

--- model/dbms.py
+++ model/dbms.py
@@ -103,6 +103,10 @@ class DBMS:
         self.Cursor.execute(f"SELECT * FROM bmw.car")
         data = self.Cursor.fetchall()
         return data
+    def getOrdersByCusId(self, cusID):
+        self.Cursor.execute(f"SELECT * FROM bwm.order WHERE cusID = '{cusID}'")
+        data = self.Cursor.fetchall()
+        return data
     def getModelsDetailByType(self, type):
         self.Cursor.execute(f"SELECT * FROM bmw.car WHERE series_name LIKE '%{type}%'")
         data = self.Cursor.fetchall()
